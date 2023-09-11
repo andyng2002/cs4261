c41b1a67b02e905662ab835fd51033a49b705ef5
Andy Nguyen <andyng2002@gmail.com>
2023-09-10 23:48:16 -0400

    install navigation stack

--- project/App.js
+++ project/App.js
@@ -1,10 +1,23 @@
+import 'react-native-gesture-handler';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const Stack = createStackNavigator();
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Notifications" component={Notifications} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Login to Access Chat Room</Text>
       <StatusBar style="auto" />
     </View>
   );
