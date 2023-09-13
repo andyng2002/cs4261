451a435a83be375205a9f727144256f04df173b7
jimmy2933 <hoangphillip121@gmail.com>
2023-09-12 20:21:41 -0400

    Updated login and create account screen UI

--- project/screens/AccountCreationScreen.js
+++ project/screens/AccountCreationScreen.js
@@ -14,7 +14,8 @@ const AccountCreationScreen = ({ navigation }) => {
   const inputStyle = {
     width: 300,
     height: 40,
-    borderColor: 'gray',
+    borderColor: '#e3e3e3',
+    backgroundColor: '#e3e3e3',
     borderWidth: 1,
     borderRadius: 5,
     marginBottom: 10,
@@ -39,43 +40,115 @@ const AccountCreationScreen = ({ navigation }) => {
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <TextInput
+      <Text style={styles.title}>Create Your Account</Text>
+        <TextInput
         style={inputStyle}
         placeholder="First Name"
         value={firstName}
         onChangeText={(text) => setFirstName(text)}
-      />
-      <TextInput
+        />
+        <TextInput
         style={inputStyle}
         placeholder="Last Name"
         value={lastName}
         onChangeText={(text) => setLastName(text)}
-      />
-      <TextInput
+        />
+        <TextInput
         style={inputStyle}
         placeholder="Email"
         value={email}
         onChangeText={(text) => setEmail(text)}
         keyboardType="email-address"
         autoCapitalize="none"
-      />
-      <TextInput
+        />
+        <TextInput
         style={inputStyle}
         placeholder="Password"
         value={password}
         onChangeText={(text) => setPassword(text)}
         secureTextEntry
-      />
-      <Button
-        title="Create Account"
+        />
+        <TouchableOpacity
+        style={styles.createAccountButton}
         onPress={handleCreateAccount}
-      />
-      <Button
-        title="Go back"
-        onPress={() => navigation.goBack()}
-      />
+        >
+        <Text style={styles.buttonText}>Create Account</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.goBack()}>
+            <Text style={styles.goBackButtonText}>Go back</Text>
+        </TouchableOpacity>
+
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f4f4f4',
+        padding: 20,
+    },
+
+    goBackButton: {
+        backgroundColor: '#f4f4f4',  
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    goBackButtonText: {
+        color: '#a741fa',  
+        fontSize: 16,
+        fontWeight: 'bold',
+    },  
+
+    createAccountButton: {
+        width: 300,
+        borderWidth: 2,
+        borderColor: '#a741fa',
+        backgroundColor: '#a741fa',
+        borderRadius: 5,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#333',
+    },
+
+    input: {
+        width: 300,
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 10,
+        paddingLeft: 10,
+        backgroundColor: '#fff',
+    },
+
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 50,
+    },
+});
 
 export default AccountCreationScreen;
